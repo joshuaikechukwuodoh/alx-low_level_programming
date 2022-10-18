972352798b4aafc88832bd04ca7cd00db1da187a
joshuaikechukwuodoh <odohjoshua82gmail.com>
2022-10-18 10:53:57 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabelt in lowercase
+ * is written by joshua
+ */
+
+void print_alphabet(void)
+
+{
+
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+
+_putchar(letter);
+
+_putchar('\n');
+
+}
