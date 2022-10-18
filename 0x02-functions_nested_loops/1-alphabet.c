3da2a068b37188f948eb6cbb0159e57f41e0ef7a
joshuaikechukwuodoh <odohjoshua82gmail.com>
2022-10-18 11:12:31 -0700

    adding ;

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,13 @@
 
 /**
  * print_alphabet - print all alphabelt in lowercase
- * is written by joshua
+ * is wriiten by joshua
  */
 
 void print_alphabet(void)
-
 {
-
 char letter;
-
-for (letter = 'a'; letter <= 'z'; letter++)
-
+for (letter = 'a'; letter <= 'z'; letter++);
 _putchar(letter);
-
 _putchar('\n');
-
 }
