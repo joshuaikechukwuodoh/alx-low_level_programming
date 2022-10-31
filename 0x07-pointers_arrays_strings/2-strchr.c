200c38e8bd1be5abfcf70d4af694f0adf4308fec
joshuaikechukwuodoh <odohjoshua82gmail.com>
2022-10-31 05:11:44 -0700

    task 2 add

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * _strchr - a function that locates a character in a string.
- *an input string to search in
- *an input character to locate into string s
- *Return: returns pointer to c position
+* _strchr - a function that locates a character in a string.
+ * @s: an input string to search in
+ * @c: an input character to locate into string s
+ * Return: returns pointer to c position
  */
 char *_strchr(char *s, char c)
 {
