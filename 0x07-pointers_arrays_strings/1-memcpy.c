1a033c03f73c48df87f9316f35e9796835a17e6b
joshuaikechukwuodoh <odohjoshua82gmail.com>
2022-10-31 05:07:03 -0700

    task 1 add

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,9 +2,9 @@
 
 /**
  * _memcpy - a function that copys memory area
- *memory area destination to copy
- *memory area source to copy from
- *number of bytes
+ * @dest: memory area destination to copy
+ * @src: memory area source to copy from
+ * @n: number of bytes
  * Return: returns pointer to copy location
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
